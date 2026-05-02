c84a442ca4b85c43002ba89c2f167ab3dbf5db9a
bmaupin <bmaupin@users.noreply.github.com>
2026-05-02 18:38:48 -0400

    feat: Delete outpost founded reaction
    ... instead of modifying it. This makes it so that the negative reaction dialogue doesn't even happen in addition to avoiding the hit to respect.

--- src/SQL/Reactions.sql
+++ src/SQL/Reactions.sql
@@ -1,9 +1,6 @@
--- The AI will still make snarky comments when cities are founded too close to them, but
--- at least we can remove the hit to respect. Removing the comment would involve
--- overriding DefaultReactionHelpers.lua, which we want to avoid.
-UPDATE Reactions
-SET
-  RespectChange = 0
+-- This prevents the negative reaction dialogue and the hit to respect from AI players
+-- when cities are founded.
+DELETE FROM Reactions
 WHERE Type = 'REACTION_OUTPOST_FOUNDED_DISLIKE'
 -- Only apply for Rising Tide
 AND EXISTS (SELECT Description FROM Civilizations WHERE Type = 'CIVILIZATION_CHUNGSU');
